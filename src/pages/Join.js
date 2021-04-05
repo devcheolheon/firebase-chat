64793805d7094b659bdd053f89c0aa1d943dd029
cheolheonshin <cheolheonshin@gmail.com>
2021-04-05 16:07:47 +0900

    [feature] 회원가입 기본 동작 구현
    회원가입 -> 로딩 -> 성공

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,8 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
+import { useHistory } from "react-router-dom";
 import styles from "../bootstrap/join.module.css";
+import Loading from "../components/common/Loading";
+
+const authJoin = () => {
+  return new Promise((resolve) => setTimeout(() => resolve(true), 3000));
+};
+const authSaveUser = () => {
+  return new Promise((resolve) => setTimeout(() => resolve(true), 3000));
+};
 
 const Join = () => {
-  return (
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const join = useCallback(async (body) => {
+    setLoading(true);
+    try {
+      let uid = await authJoin(body);
+      await authSaveUser({ ...body, uid });
+      history.push("/createChat");
+    } catch (e) {
+      alert("error");
+    }
+    setLoading(false);
+  }, []);
+
+  return loading ? (
+    <Loading></Loading>
+  ) : (
     <main className={styles.join}>
       <form>
         <h1 class="h3 mb-3 fw-normal">가입하기</h1>
@@ -12,6 +41,8 @@ const Join = () => {
             class="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
           <label for="floatingInput">이메일</label>
         </div>
@@ -21,6 +52,8 @@ const Join = () => {
             class="form-control"
             id="floatingPassword"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            vaule={password}
           />
           <label for="floatingPassword">비밀번호</label>
         </div>
@@ -30,11 +63,17 @@ const Join = () => {
             class="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            onChange={(e) => setNickname(e.target.value)}
+            vaule={nickname}
           />
           <label for="floatingInput">닉네임</label>
         </div>
 
-        <button class="w-100 btn btn-lg btn-primary" type="submit">
+        <button
+          class="w-100 btn btn-lg btn-primary"
+          type="submit"
+          onClick={() => join({ email, password, nickname })}
+        >
           가입
         </button>
       </form>
