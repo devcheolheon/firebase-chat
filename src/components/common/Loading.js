229b597199c6a72a08edbaef269397d8225f5a8f
cheolheonshin <cheolheonshin@gmail.com>
2021-04-05 15:31:44 +0900

    [feature] 로그인 페이지 로그인 기본 동작 구현 (firebase 적용 전)
    로그인 -> 로딩 -> 성공 .. api 미적용 버젼

---
+++ src/components/common/Loading.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Loading = () => <div className="spinner-border" role="status"></div>;
+
+export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,15 +2,53 @@ import React, { useState, useEffect, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import logo from "../logo.png";
 import styles from "../bootstrap/login.module.css";
+import Loading from "../components/common/Loading";
+
+const authLogin = () => {
+  return new Promise((resolve) => setTimeout(() => resolve(true), 4000));
+};
+
+const authLogout = () => {
+  Promise.resolve(true);
+};
 
 const Login = () => {
   const history = useHistory();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(false);
 
   const linktoJoin = useCallback(() => {
     history.push("/join");
   }, []);
 
-  return (
+  const validateForm = useCallback(() => {
+    // todo : 로그인 폼 validation
+    return true;
+  }, {});
+
+  const login = useCallback(async (email, password) => {
+    if (!validateForm()) return false;
+    setLoading(true);
+
+    try {
+      await authLogin(email, password);
+      setLoginStatus(true);
+    } catch (e) {
+      if (e.code == "auth/user-not-found") {
+        alert("가입하세요");
+      }
+    }
+
+    setLoading(false);
+    history.push("/createChat");
+  }, []);
+
+  return loading ? (
+    <Loading></Loading>
+  ) : (
     <main className={styles.login}>
       <form>
         <img class="mb-4" src={logo}></img>
@@ -21,6 +59,8 @@ const Login = () => {
             class="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            value={email}
+            onChange={(evt) => setEmail(evt.target.value)}
           />
           <label for="floatingInput">이메일</label>
         </div>
@@ -30,11 +70,19 @@ const Login = () => {
             class="form-control"
             id="floatingPassword"
             placeholder="Password"
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
           />
           <label for="floatingPassword">비밀번호</label>
         </div>
 
-        <button class="w-100 btn btn-lg btn-primary" type="submit">
+        <button
+          class="w-100 btn btn-lg btn-primary"
+          type="submit"
+          onClick={() => {
+            login(email, password);
+          }}
+        >
           로그인
         </button>
         <p
