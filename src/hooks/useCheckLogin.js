29912e1c997a1b84665845f0809f97a3a6a5570a
cheolheonshin <cheolheonshin@gmail.com>
2021-04-16 13:18:56 +0900

    [feature] login 상태 확인 hook의 코드 일부 고침

--- src/hooks/useCheckLogin.js
+++ src/hooks/useCheckLogin.js
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { firebaseApp } from "../firebase";
 import { useHistory } from "react-router-dom";
 
 const useCheckLogin = ({ loginUrl, logoutUrl }) => {
-  const login_loading = useSelector((state) => state.auth.loading);
-  const isLogin = useSelector((state) => state.auth.isLogin);
-  const uid = useSelector((state) => state.auth.uid);
-  const nickname = useSelector((state) => state.auth.nickname);
+  const { login_loading, isLogin, uid, nickname } = useSelector(
+    (state) => state.auth
+  );
 
   const history = useHistory();
 
@@ -16,7 +14,7 @@ const useCheckLogin = ({ loginUrl, logoutUrl }) => {
       if (isLogin && loginUrl) history.push(loginUrl);
       if (!isLogin && logoutUrl) history.push(logoutUrl);
     }
-  }, [login_loading]);
+  }, [login_loading, isLogin]);
 
   return [login_loading, isLogin, uid, nickname];
 };
