6ac9e2f7b3a2909a1fded74f0d3743fa5ab49546
cheolheonshin <cheolheonshin@gmail.com>
2021-04-17 09:18:10 +0900

    [chore] 사용자에 저장된 password 일괄 삭제
    firebase의 batch 기능을 활용
    .. 데이터 양이 많을때는 서버 클라이언트 api를 쓸것
    .. 함수는 주석 처리

--- src/firebaseUtils/users.js
+++ src/firebaseUtils/users.js
@@ -10,3 +10,28 @@ export async function getUserNameById(id) {
   }
   return "noname";
 }
+
+export async function getUsers() {
+  const userRef = db.collection("google");
+  const snapshot = await userRef.get();
+  let users = [];
+  snapshot.forEach((doc) => users.push(doc.data()));
+  return users;
+}
+
+/*
+//  user 정보에 저장된 password를 일괄적으로 삭제하는 코드 
+//  user 일괄 작업
+async function batchUser() {
+  let batch = db.batch();
+  let querySnapshot = await db.collection("google").get();
+  querySnapshot.forEach((userRef) => {
+    batch.update(userRef.ref, {
+      password: firebase.firestore.FieldValue.delete(),
+    });
+  });
+  batch.commit().then(() => console.log("complete"));
+}
+batchUser();
+
+*/
