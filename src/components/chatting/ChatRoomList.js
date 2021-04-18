51130a3bdff776290085d5eb2996257f1e744eb1
cheolheonshin <cheolheonshin@gmail.com>
2021-04-18 20:35:33 +0900

    [fix] 채팅방 클릭시 발생 에러 수정

--- src/components/chatting/ChatRoomList.js
+++ src/components/chatting/ChatRoomList.js
@@ -111,6 +111,7 @@ const ChatRoomList = ({ chats, onClickHandler, selectedChat }) => {
     <List
       onClick={(e) => {
         const li = e.target.closest("li");
+        if (!li) return;
         const id = li.dataset.id;
         onClickHandler(id);
       }}
