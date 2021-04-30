48331c858827d62041520ca3d2a2fbef481b46fc
cheolheonshin <cheolheonshin@gmail.com>
2021-04-30 10:27:10 +0900

    [feature] 사용자 정보 그래프에 실시간 변화하는 데이터에 동적으로 그래프가 변화하는 기능 추가
    매번 새로 렌더링 하지 않고 추가되는 데이터를 에니메이션 과 함꼐 추가함

--- src/graphUtils/usersRelationGraph.js
+++ src/graphUtils/usersRelationGraph.js
@@ -33,67 +33,84 @@ const drag = (simulation) => {
     .on("end", dragended);
 };
 
+let svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+
+let link = svg
+  .append("g")
+  .attr("stroke", "#999")
+  .attr("stroke-opacity", 0.6)
+  .selectAll("line");
+
+let node = svg
+  .append("g")
+  .attr("stroke", "#fff")
+  .attr("stroke-width", 1.5)
+  .selectAll("circle");
+
+let text = svg
+  .attr("font-size", "11px")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .selectAll("text");
+
+let ticked = () => {
+  console.log(link, node, text);
+  link
+    .attr("x1", (d) => d.source.x)
+    .attr("y1", (d) => d.source.y)
+    .attr("x2", (d) => d.target.x)
+    .attr("y2", (d) => d.target.y);
+
+  node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+
+  text.attr("x", (d) => d.x).attr("y", (d) => d.y + 20);
+};
+
+const simulation = d3
+  .forceSimulation()
+  .force("charge", d3.forceManyBody())
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force(
+    "link",
+    d3.forceLink().id((d) => d.id)
+  )
+  .force("x", d3.forceX())
+  .force("y", d3.forceY())
+  .on("tick", ticked);
+
 export const usersRelationGraph = (target, data) => {
-  const links = data.links.map((d) => Object.create(d));
-  const nodes = data.nodes.map((d) => Object.create(d));
+  const update = (data) => {
+    if (data.nodes.length == 0 && data.links.length == 0) return;
+    const old = new Map(node.data().map((d) => [d.id, d]));
+    let nodes = data.nodes.map((d) => Object.assign(old.get(d.id) || {}, d));
+    let links = data.links.map((d) => Object.create(d));
 
-  const simulation = d3
-    .forceSimulation(nodes)
-    .force(
-      "link",
-      d3.forceLink(links).id((d) => d.id)
-    )
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    node = node
+      .data(nodes, (d) => d.id)
+      .join((enter) => enter.append("circle").attr("r", 3).attr("fill", color))
+      .call(drag(simulation));
 
-  const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+    link = link
+      .data(links, (d) => d.source + d.target)
+      .join("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
 
-  const text = svg
-    .selectAll("text")
-    .data(nodes)
-    .join("text")
-    .attr("font-size", "11px")
-    .attr("text-anchor", "middle")
-    .attr("fill", "white")
-    .attr("x", (d) => d.x) // Flipped X
-    .attr("y", (d) => d.y + 20) // Flipped Y
-    .text((d) => d.name)
-    .call(drag(simulation));
+    text = text
+      .data(nodes, (d) => d.id)
+      .join("text")
+      .attr("x", (d) => d.x) // Flipped X
+      .attr("y", (d) => d.y + 20) // Flipped Y
+      .text((d) => d.name);
 
-  const link = svg
-    .append("g")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line")
-    .attr("stroke-width", (d) => Math.sqrt(d.value));
+    simulation.nodes(nodes);
+    simulation.force("link").links(links);
+    simulation.alpha(1).restart().tick();
+    ticked();
+  };
 
-  const node = svg
-    .append("g")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-    .attr("r", 3)
-    .attr("fill", color)
-    .call(drag(simulation));
-
-  node.append("title").text((d) => d.id);
-
-  simulation.on("tick", () => {
-    link
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y);
-
-    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-
-    text.attr("x", (d) => d.x).attr("y", (d) => d.y + 20);
-  });
-
+  if (target.innerHTML) return update;
   // invalidation.then(() => simulation.stop());
+
   target.appendChild(svg.node());
+  return update;
 };
